a3a4432302c830a59f03b5b2d11846823286b84d
GreenNeptune <silentwisdom75@gmail.com>
2020-01-15 15:42:26 +0200

    add quote schema

---
+++ models/quote.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+//  crete quote schema 
+
+const QuoteSchema = new Schema({
+  author: {
+    type: String,
+    required: [true, 'author is required']
+  },
+  text: {
+    type: String,
+    required: [true, 'text is required']
+  }
+})
+
+//  create collection called 'quotes' in db, 
+//  create correspond schema to 'quotes' collection   
+const Quote = mongoose.model('quote', QuoteSchema);
+
+
+module.exports = Quote
