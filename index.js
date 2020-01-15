f686f744abbe400d955f9740a13554f9afed2256
GreenNeptune <silentwisdom75@gmail.com>
2020-01-15 16:35:43 +0200

    connect to db

--- index.js
+++ index.js
@@ -2,12 +2,27 @@ require('dotenv').config();
 
 const express = require('express');
 const expressRouter = require ('./routes/api')
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 // create express app
 const app = express();
 
+// connect to db, create db if doesn't exists
+mongoose.connect(process.env.DATABASE_URL,{ useNewUrlParser: true, useUnifiedTopology: true }, ()=>{
+  console.log('connected to db');
+});
+
+// overriding mongoose promise with node promise
+// cuurently mongoose promise is deprecated
+mongoose.Promise = global.Promise
+
+
 // routes middleware 
 app.use('/api', expressRouter);
+
+// body parser can be replace with app.use(express.json()) at express@4.16.0 package
+app.use(bodyParser.json())
 
 app.listen(process.env.SERVER_PORT || 4000, function(){
   console.log(`start server on port :${process.env.SERVER_PORT}`);
